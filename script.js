309be911a7a8455f6596ce6d1d4c442c2dd5b6f0
Najaf Sharifov <nwerifov@mail.ru>
2023-03-06 00:46:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//Task 2.9
+//1
+
+/*let twoDigitNumber = +prompt("Enter two digit number");
+while(twoDigitNumber = 100) {
+twoDigitNumber+=7;
+console.log(twoDigitNumber);
+}*/
+
+//2
+
+/*let message = "I know how to use cycles";
+let num = +prompt("Enter random number");
+for(let i = 0; i<num;i++) {
+    console.log(message);
+}*/
+
+//3
+/*let num = 100;
+while(num<990) {
+  num+=10;
+  console.log(num);
+}*/
+
+//4
+
+/*let sum = 0;
+
+for (let i = 11; i <= 99; i++) {
+  if (i % 2 == 1) {
+    sum += i;
+    console.log("сумма нечетных двузначных чисел = " + sum);
+  }
+}*/
+
